081b1433039c3284d8562e9c977a0f26e62a2eae
Pat <p171982@gmail.com>
2021-12-29 20:01:56 +0000

    markdown

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -28,7 +28,7 @@ class QuestionController extends AbstractController
     public function show($slug)
     {
         $answers = [
-            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
